1fe78fdbe6403a8de6107aaef6f8671fcac745d1
LuzBGouveia <lu.barros.gouveia2@gmail.com>
2025-10-30 15:47:10 -0300

    Adicionando o projeto ao respositório.

---
+++ src/main/java/br/com/fiap/bo/PokemonBO.java
@@ -0,0 +1,44 @@
+package br.com.fiap.bo;
+
+import br.com.fiap.dao.PokemonDAO;
+import br.com.fiap.to.PokemonTO;
+
+import java.util.ArrayList;
+
+public class PokemonBO {
+    private PokemonDAO pokemonDAO;
+
+    public ArrayList<PokemonTO> findAll() {
+        pokemonDAO = new PokemonDAO();
+        // aqui se implementa a regra de negócios
+        return pokemonDAO.findAll();
+    }
+
+    public PokemonTO findByCodigo(Long codigo) {
+        pokemonDAO = new PokemonDAO();
+        // aqui se implementa a regra de negócios
+        return pokemonDAO.findByCodigo(codigo);
+    }
+
+    public PokemonTO save (PokemonTO pokemon) {
+        pokemonDAO = new PokemonDAO();
+        // aqui se implementa a regra de negócios
+        // verificando se o remédio já está vencido
+        /*if (pokemon.getDataDeValidade().isBefore(LocalDate.now())) {
+            return null;
+        }*/
+        return pokemonDAO.save(pokemon);
+    }
+
+    public boolean delete(Long codigo) {
+        pokemonDAO = new PokemonDAO();
+        // aqui se implementa a regra de negócios
+        return pokemonDAO.delete(codigo);
+    }
+
+    public PokemonTO update(PokemonTO pokemon) {
+        pokemonDAO = new PokemonDAO();
+        //Aqui se implementa a regra de negócios
+        return pokemonDAO.update(pokemon);
+    }
+}
---
+++ src/main/java/br/com/fiap/to/PokemonTO.java
@@ -0,0 +1,82 @@
+package br.com.fiap.to;
+
+import jakarta.validation.constraints.*;
+
+import java.time.LocalDate;
+
+public class    PokemonTO {
+    // atributo
+    private Long codigo;
+    @NotBlank
+    private String nome;
+    @NotNull
+    @PositiveOrZero
+    private Double altura;
+    @NotNull
+    @PositiveOrZero
+    private Double peso;
+    @NotBlank
+    private String categoria;
+    @PastOrPresent
+    private LocalDate dataDaCaptura;
+    // construtores
+    public PokemonTO() {
+    }
+    public PokemonTO(Long codigo, String nome, Double altura, Double peso, String categoria, LocalDate dataDaCaptura) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.altura = altura;
+        this.peso = peso;
+        this.categoria = categoria;
+        this.dataDaCaptura = dataDaCaptura;
+    }
+    // métodos getters/setters
+
+    public Long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Long codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(Double altura) {
+        this.altura = altura;
+    }
+
+    public Double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(Double peso) {
+        this.peso = peso;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public LocalDate getDataDaCaptura() {
+        return dataDaCaptura;
+    }
+
+    public void setDataDaCaptura(LocalDate dataDaCaptura) {
+        this.dataDaCaptura = dataDaCaptura;
+    }
+}
